7cd0e544be857f427068de97bebd6bef7a7155fb
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-11-01 15:02:16 -0600

    steve moved all auth initialization info into a separate file

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase';
+import firebase from '../firebase';
 
 // const firebaseApp = {
 //   apiKey: 'AIzaSyDwnsJcOAy2cP2lyTFdWcanOMTf2DRZpIs',
--- src/index.js
+++ src/index.js
@@ -11,21 +11,10 @@ import App from './components/App';
 import DetailedPin from './components/DetailedPin';
 import AddingRecommendation from './containers/AddingRecommendation';
 import * as actions from './actions/auth';
-import firebase from 'firebase';
+import firebase from './firebase';
+
 // const middleware = [thunk];
 require('!style!css!sass!./styles/main.scss');
-
-const firebaseApp = {
-  apiKey: 'AIzaSyDwnsJcOAy2cP2lyTFdWcanOMTf2DRZpIs',
-  authDomain: 'local-cb336.firebaseapp.com',
-  databaseURL: 'https://local-cb336.firebaseio.com',
-  storageBucket: 'local-cb336.appspot.com',
-  messagingSenderId: '514582937003'
-};
-
-console.log('WHAAAT');
-
-firebase.initializeApp(firebaseApp);
 
 const enhancers = compose(
   applyMiddleware(thunk),
