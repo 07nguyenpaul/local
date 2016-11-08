c176b6018bc4e917452dd124618da5652862d217
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-11-07 20:03:42 -0700

    stuck on how to grab info from mulitple searches through api without replacing state

--- src/containers/AddingRecommendation.js
+++ src/containers/AddingRecommendation.js
@@ -1,15 +1,19 @@
 import { connect } from 'react-redux';
 import {submitNewRecommendation} from '../actions/recommendationFirebase';
 import AddingRecommendation from '../components/AddingRecommendation';
+import * as actions from '../actions/recommendationFirebase';
+import { bindActionCreators } from 'redux';
 
 const mapStateToProps= (state) =>{
   return state
 };
 
+// const mapDispatchToProps= (dispatch) => {
+//   return bindActionCreators(actions, dispatch);
+// };
 const mapDispatchToProps= (dispatch) => {
   return {
     onSubmit: (content) => {
-      // dispatch(fetchFood(content.name));
       dispatch(submitNewRecommendation(content));
     }
   };
