a42337b39fa65cc605d9ff2278ad13485d37f35c
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-11-09 23:07:51 -0700

    added test suite for app

---
+++ src/tests/add.spec.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import AddingRecommendation from '../components/AddingRecommendation';
+// import {shallow, mount} from 'enzyme';
+// import {expect} from 'chai';
+//
+//
+// describe('Add', () => {
+//   it('should add a new recommendation', () => {
+//
+//   });
+// });
